a8b6c85b00cd7c6039b101f9264502e023ac2ede
mannyrobin <69haiqian@gmail.com>
2017-09-24 17:50:44 +0200

    Fixed hardcoded port and adress

--- main.go
+++ main.go
@@ -4,17 +4,20 @@
 // SOURCES
 // handler test: 	https://elithrar.github.io/article/testing-http-handlers-go/
 // API use(JSON): 	https://blog.alexellis.io/golang-json-api-client/
+// Get port nr
 // Golang's documentation pages
 
 package main
 
 import (
 	"net/http"
+	"os"
 )
 
 
 func main() {
+	port := os.Getenv("PORT")
 	http.HandleFunc("/projectinfo/v1/", HandlerGitURL)
 	http.HandleFunc("/", HandlerWrongURL)
-	http.ListenAndServe("localhost:8080", nil)
+	http.ListenAndServe(":" + port, nil)
 }
